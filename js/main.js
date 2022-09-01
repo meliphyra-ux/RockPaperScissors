89c863437176ebce6585c61ddf44ac751f79de90
meliphyra-ux <83269567+meliphyra-ux@users.noreply.github.com>
2022-09-01 03:23:15 +0300

    Finished version

---
+++ js/main.js
@@ -0,0 +1,44 @@
+function computerPlay(){
+    //  Creating random number from 0 to 1 and multiply it by 100, then clearing decimals
+    const choice = Math.floor(Math.random() * 100)
+    if(choice <= 33){
+        return "Scissors"
+    }
+    if(choice > 33 && choice <= 66){
+        return "Paper"
+    }
+    else{
+        return "Rock"
+    }
+}
+function playRockPaperScissors(playerSelection){
+    // Checking if playerMade a selection
+    const playerSelectionLowercase = playerSelection ? playerSelection.toLowerCase() : "Not correct value";
+    // Checking if selection is rock, paper or scissors
+    if(playerSelectionLowercase !== "rock" && playerSelectionLowercase !=="scissors" && playerSelectionLowercase !== "paper"){
+        return "Invalid choice. Try using Rock, Paper or Scissors"
+    }
+    // Grabbing selection from computer and checking if selections are equal
+    const computerSelection = computerPlay()
+    if(computerSelection.toLowerCase() === playerSelectionLowercase){
+        return `Your choice - ${playerSelection}, computers choice - ${computerSelection}. Tie🏳`
+    }
+    // Checking winning combinations
+    switch(playerSelectionLowercase){
+        case "scissors":
+            return `Your choice - ${playerSelection}, computers choice - ${computerSelection}. ${computerSelection === "Rock" ? "You lost❌" : "You win✔"}`;
+            break;
+        case "paper":
+            return `Your choice - ${playerSelection}, computers choice - ${computerSelection}. ${computerSelection === "Scissors" ? "You lost❌" : "You win✔"}`;
+            break;
+        case "rock":
+            return `Your choice - ${playerSelection}, computers choice - ${computerSelection}. ${computerSelection === "Paper" ? "You lost❌" : "You win✔"}`;
+            break;
+    }
+}
+// Invoking game for users
+for(let i = 0; i < 5; i++)
+{
+    const playerSelection = prompt("Please right your choice (Rock, Paper, Scissors)", "Scissors")
+    console.log(playRockPaperScissors(playerSelection))
+}
